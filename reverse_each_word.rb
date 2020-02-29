c47319b9b601b0903992c9bec6978cd7c0c2370e
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-29 16:57:28 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,30 @@
+def reverse_each_word(str)
+  array = str.split(/ /)
+  new_array = []
+  i = 0
+  while i < array.length do
+    new_array << array[i].reverse
+    i+=1
+  end
+  result = new_array.join(" ")
+  p result
+end
+
+
+
+# reverse_each_word("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b.reverse} on #{"bread".reverse}" }
+
+# describe '#reverse_each_word' do
+#   let(:sentence1) { "Hello there, and how are you?" }
+#   it 'reverses all the words in a string without reversing the order of the words' do
+#     expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
+#   end
+
+#   let(:sentence2) { "Hi again, just making sure it's reversed!" }
+#   it 'reverses all the words in another string without reversing the order of the words' do
+#     expect(reverse_each_word(sentence2)).to eq("iH ,niaga tsuj gnikam erus s'ti !desrever")
+#   end
+
+#   it 'uses collect' do
+#     expect_any_instance_of(Array).to receive(:collect).and_call_original
+#     reverse_each_word("Verifying that collect is being called.")
