ea7a9676e25228ded9835602617cd10b3a0e8e61
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-03-02 17:21:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-# def reverse_each_word(str)
-#   str.split.collect {|word| word.reverse}.join(" ")
-# end
+def reverse_each_word(str)
+  str.split.collect {|word| word.reverse}.join(" ")
+end
 
 
 # def reverse_each_word(str)
